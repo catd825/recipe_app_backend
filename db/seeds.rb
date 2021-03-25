57244efe87a7216b66371659b17e4fef3faa9512
catd825 <60204063+catd825@users.noreply.github.com>
2021-03-25 16:45:58 -0400

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,8 +14,8 @@ user2 = User.create!(name: 'David', username:'ddubbs87', password_digest: 'passw
 user3 = User.create!(name: 'Mom', username:'Nonna1956', password_digest: 'password', bio: 'i <3 italian food')
 
 recipe1 = Recipe.create!(title: 'pasta', description: 'basic pasta recipe', ingredients: '1 lb of pasta', instructions: 'boi l water, add pasta, cook for 8 min, then drain, add toppings, and serve', img_url: 'https://pinchofyum.com/wp-content/uploads/Vegan-Vodka-Pasta-Square.jpg', recipe_creator_id: user3.id)
-recipe2 = Recipe.create!(title: 'pizza', description: 'pizza pizza', ingredients: 'dough, pizza sauce, cheese', instructions: 'bake!', img_url: '', recipe_creator_id: user3.id)
-recipe3 = Recipe.create!(title: 'sauce', description: 'best marinara!', ingredients: 'canned tomato, olive oil, oregano', instructions: 'simmer for hours!', img_url: '', recipe_creator_id: user3.id)
+recipe2 = Recipe.create!(title: 'pizza', description: 'pizza pizza', ingredients: 'dough, pizza sauce, cheese', instructions: 'bake!', img_url: 'https://relish.com/cdn-cgi/image/fit=cover,f=auto,dpr=2,w=254,h=330/https://cookieandkate.com/images/2018/10/greek-pizza-recipe-2.jpg', recipe_creator_id: user3.id)
+recipe3 = Recipe.create!(title: 'sauce', description: 'best marinara!', ingredients: 'canned tomato, olive oil, oregano', instructions: 'simmer for hours!', img_url: 'https://relish.com/cdn-cgi/image/fit=cover,f=auto,dpr=2,w=254,h=330/https://www.seriouseats.com/recipes/images/2011/08/20110814-166086-marinara.jpg', recipe_creator_id: user3.id)
 
 FavoriteRecipe.create!(recipe_liker_id: user2.id, recipe_id: recipe2.id, favorited: true)
 FavoriteRecipe.create!(recipe_liker_id: user1.id, recipe_id: recipe2.id, favorited: true)
