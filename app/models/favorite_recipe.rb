faa0cc93b8d425deec930db5d16a7da7a1e5221c
catd825 <60204063+catd825@users.noreply.github.com>
2021-05-01 21:24:13 -0400

    updated dependent destroy on models

--- app/models/favorite_recipe.rb
+++ app/models/favorite_recipe.rb
@@ -1,5 +1,5 @@
 class FavoriteRecipe < ApplicationRecord
     # this returns all users who have liked a recipe
     belongs_to :recipe_liker, class_name: :User, foreign_key: "user_id", optional: true, dependent: :destroy
-    belongs_to :recipe
+    belongs_to :recipe, dependent: :destroy
 end
